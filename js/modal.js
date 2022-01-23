eb30c1d36431660df9496b0abe414d2b9917269f
morysol <cepera.cmuphob@gmail.com>
2022-01-23 16:45:23 +0200

    js
    added

---
+++ js/modal.js
@@ -0,0 +1,12 @@
+// try to remove class is-hidden
+function purchaseService() {
+  var element = document.getElementById('backdrop');
+  element.classList.remove('is-hidden');
+}
+
+// try to add class is-hidden and close modal window
+
+function closeModal() {
+  var element = document.getElementById('backdrop');
+  element.classList.add('is-hidden');
+}
